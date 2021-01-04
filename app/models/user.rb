9964949350c74578cf8573baf79b617e88e47d45
GitHayato <gabayt0_0@icloud.com>
2021-01-04 11:48:54 +0900

    test_code

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  EMAIL_REGEX = /@.+/.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
   validates :nickname,        presence: true
-  validates :first_name,      presence: true
-  validates :last_name,       presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana,  presence: true
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/ }
+  validates :last_name_kana,  presence: true, format: { with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/ }
   validates :birthday,        presence: true
-  
+
+  validates_format_of :email, with: EMAIL_REGEX 
+  validates_format_of :password, with: PASSWORD_REGEX
 end
