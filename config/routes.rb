15b7d145996659b68c2db2e6baa5ad8e247a82ff
GitHayato <gabayt0_0@icloud.com>
2021-01-15 14:55:45 +0900

    routing&g controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'purchases/index'
   devise_for :users
   
   root to: "items#index" 
 
   resources :items, except: :index
+  resources :purchases, only: :index
 end
