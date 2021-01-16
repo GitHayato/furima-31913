77ca3ac30012f6c84d74e58ee1ef318272d65565
GitHayato <gabayt0_0@icloud.com>
2021-01-16 12:13:57 +0900

    fix

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,4 +1,4 @@
-class Purchase
+class PurchaseForm
   include ActiveModel::Model
   attr_accessor :nickname, :email, :encrypted_password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, :image, :item_name, :explanation, :condition_id, :category_id, :delivery_fee_id, :prefecture_id, :preparation_id, :price, :postal_code, :prefecture_id, :district, :address :building_name, :phone_number
 
