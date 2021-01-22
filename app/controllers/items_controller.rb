91036b128eaa28e3f162e608f6f3bf131277da0c
GitHayato <gabayt0_0@icloud.com>
2021-01-22 15:53:10 +0900

    fix from review

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.id != @item.user_id or @purchase
+    if current_user.id != @item.user_id || @purchase
       redirect_to root_path
     end
   end
--- spec/models/purchase_form_spec.rb
+++ spec/models/purchase_form_spec.rb
@@ -9,68 +9,72 @@ RSpec.describe PurchaseForm, type: :model do
       item.save
       @purchase_form = FactoryBot.build(:purchase_form, user_id: user.id, item_id: item.id)
     end
-    
-    it '商品購入ができること' do
-      expect(@purchase_form).to be_valid
-    end
-    
-    it '建物名は空でも購入できること' do
-      @purchase_form.building_name = ''
-      expect(@purchase_form).to be_valid
-    end
-    
-    it '郵便番号が空だと購入できない' do
-      @purchase_form.postal_code = ''
-      @purchase_form.valid?
-      expect(@purchase_form.errors.full_messages).to include("Postal code can't be blank")
+
+    context '購入できる時' do
+      it '商品購入ができること' do
+        expect(@purchase_form).to be_valid
+      end
+      
+      it '建物名は空でも購入できること' do
+        @purchase_form.building_name = ''
+        expect(@purchase_form).to be_valid
+      end
     end
 
-    it '郵便番号にハイフンがないと購入できない' do
-      @purchase_form.postal_code = '1234567'
-      @purchase_form.valid?
-      expect(@purchase_form.errors.full_messages).to include("Postal code is invalid")
-    end
+    context '購入できない時' do
+      it '郵便番号が空だと購入できない' do
+        @purchase_form.postal_code = ''
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Postal code can't be blank")
+      end
 
-    it '都道府県の選択をしていないと購入できない' do
-      @purchase_form.prefecture_id = 1
-      @purchase_form.valid?
-      expect(@purchase_form.errors.full_messages).to include("Prefecture must be other than 1")
-    end
+      it '郵便番号にハイフンがないと購入できない' do
+        @purchase_form.postal_code = '1234567'
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Postal code is invalid")
+      end
 
-    it '地域が空だと購入できない' do
-      @purchase_form.district = ''
-      @purchase_form.valid?
-      expect(@purchase_form.errors.full_messages).to include("District can't be blank")
-    end
+      it '都道府県の選択をしていないと購入できない' do
+        @purchase_form.prefecture_id = 1
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Prefecture must be other than 1")
+      end
 
-    it '番地が空だと購入できない' do
-      @purchase_form.address = ''
-      @purchase_form.valid?
-      expect(@purchase_form.errors.full_messages).to include("Address can't be blank")
-    end
+      it '地域が空だと購入できない' do
+        @purchase_form.district = ''
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("District can't be blank")
+      end
 
-    it '電話番号が空だと購入できない' do
-      @purchase_form.phone_number = ''
-      @purchase_form.valid?
-      expect(@purchase_form.errors.full_messages).to include("Phone number can't be blank")
-    end
+      it '番地が空だと購入できない' do
+        @purchase_form.address = ''
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Address can't be blank")
+      end
 
-    it '電話番号にハイフンがあれば購入できない' do
-      @purchase_form.phone_number = '020-1234-5678'
-      @purchase_form.valid?
-      expect(@purchase_form.errors.full_messages).to include("Phone number is too long (maximum is 11 characters)")
-    end
-    
-    it '電話番号は11桁以下でないと購入できない' do
-      @purchase_form.phone_number = '020123456789'
-      @purchase_form.valid?
-      expect(@purchase_form.errors.full_messages).to include("Phone number is too long (maximum is 11 characters)")
-    end
+      it '電話番号が空だと購入できない' do
+        @purchase_form.phone_number = ''
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Phone number can't be blank")
+      end
 
-    it 'tokenがなければ購入できない' do
-      @purchase_form.token = ''
-      @purchase_form.valid?
-      expect(@purchase_form.errors.full_messages).to include("Token can't be blank")
+      it '電話番号にハイフンがあれば購入できない' do
+        @purchase_form.phone_number = '020-1234-5678'
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Phone number is too long (maximum is 11 characters)")
+      end
+      
+      it '電話番号は11桁以下でないと購入できない' do
+        @purchase_form.phone_number = '020123456789'
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Phone number is too long (maximum is 11 characters)")
+      end
+
+      it 'tokenがなければ購入できない' do
+        @purchase_form.token = ''
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Token can't be blank")
+      end
     end
   end
 end
